f2e9743bb833f209792539696c0bab8c6a419a26
JiaYongChen <cjy461549601@gmail.com>
2026-09-04 13:09:34 +0800

    界面: 为 ConnectionCard 添加删除 SVG 图标

--- src/app/ConnectionCard.cpp
+++ src/app/ConnectionCard.cpp
@@ -2,9 +2,6 @@
 #include "ui_ConnectionCard.h"
 #include "common/theme/IconThemeProvider.h"
 
-#include <QtWidgets/QApplication>
-#include <QtWidgets/QStyle>
-
 ConnectionCard::ConnectionCard(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::ConnectionCard)
@@ -21,7 +18,7 @@ ConnectionCard::ConnectionCard(QWidget *parent)
     // 设置图标
     ui->connectButton->setIcon(IconThemeProvider::icon("connect"));
     ui->editButton->setIcon(IconThemeProvider::icon("edit"));
-    ui->deleteButton->setIcon(QApplication::style()->standardIcon(QStyle::SP_TrashIcon));
+    ui->deleteButton->setIcon(IconThemeProvider::icon("delete"));
 
     // 信号连接
     connect(ui->connectButton, &QToolButton::clicked,
